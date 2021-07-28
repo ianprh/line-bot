ccc2fcf642827b357353e64af6d70465029771cb
Ianprh <ian.prh@gmail.com>
2021-07-28 20:36:32 +0800

    update

--- app.py
+++ app.py
@@ -40,10 +40,18 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你是胖己於'
+    r = '抱歉我不懂你的意思!'
+
+    if msg in ['Hi', 'hi', 'HI', '哈囉', '你好']:
+        r = '您好!'
+    elif msg == '你吃飯了嗎?':
+        r = '還沒，你呢?'
+    elif '我' in msg:
+        r = '你是胖己於'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
